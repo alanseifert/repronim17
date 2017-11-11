1d7c294870dc41eacea4e1c840884ed6cf8d5fc6
Ely <benjamin.ely@mssm.edu>
2017-11-11 10:14:18 -0500

    updates to pullT1.sh, still WIP

--- pullT1s.sh
+++ pullT1s.sh
@@ -2,15 +2,15 @@
 
 mkdir -p Subjects/female Subjects/male
 
-subtable="" # INSERT CSV HERE
+subtable="ReproData.csv" # INSERT CSV HERE
 
-for (( i=2 ; i<=31 ; i++ )) ; do
+for (( i=2 ; i<=4 ; i++ )) ; do
 	#starting at 2 to avoid header
-	line=`head -n $i subtable | tail -n 1`
-	age=cat $line | awk '{ print $4 }'
+	line=`head -n $i $subtable | tail -n 1`
+	age=cat $line | awk -F "," '{ print $4 }' $subtable
 	if [[ "$age" -eq 18 ]] ; then
-		sex=cat $line | awk '{ print $3 }'
-		url=cat $line | awk '{ print $5 }'
+		sex=cat $line | awk -F "," '{ print $3 }'
+		url=cat $line | awk -F "," '{ print $5 }'
 		filename=${url##*/}
 		if [[ "$sex" == "F" ]] ; then
 			curl -o Subjects/female/$filename
