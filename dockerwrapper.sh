558850838b4afee11c3adadf809164c612223dba
Junqian Gordon Xu <junqian.xu@mssm.edu>
2017-11-11 10:26:49 -0500

    fix docker name and run it

--- dockerwrapper.sh
+++ dockerwrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ProjName=ReproNimChallenge
+ProjName=repronimchallenge
 
 # pull neurodocker image
 docker pull kaczmarj/neurodocker:master
@@ -14,4 +14,7 @@ docker run -it --rm kaczmarj/neurodocker:master generate \
 	--no-check-urls > ${ProjName}.Dockerfile
 
 # build docker image
-docker build -t ${ProjName} -f ${ProjName}.Dockerfile .
+docker build -t ${ProjName} -f ${ProjName}.Dockerfile ${PWD}
+
+# run docker image
+docker run -it -v ${PWD}:/data -p 9990:9990 ${ProjName}
