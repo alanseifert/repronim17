26451f9276ea9c7b1f8187866debe3714673ddec
Junqian Gordon Xu <junqian.xu@mssm.edu>
2017-11-11 12:17:21 -0500

    add processing scripts to entrypoint

--- dockerwrapper.sh
+++ dockerwrapper.sh
@@ -10,11 +10,11 @@ docker run -it --rm kaczmarj/neurodocker:master generate \
 	-b neurodebian:stretch-non-free \
 	-p apt \
 	--install fsl-core vim wget\
-	--add-to-entrypoint "source /etc/fsl/fsl.sh" \
+	--add-to-entrypoint "source /etc/fsl/fsl.sh; /repronim17/pullT1s.sh; /repronim17/bet_anat.sh" \
 	--no-check-urls > ${ProjName}.Dockerfile
 
 # build docker image
 docker build -t ${ProjName} -f ${ProjName}.Dockerfile ${PWD}
 
 # run docker image
-docker run -it -v ${PWD}:/repronim17 -p 9990:9990 ${ProjName}
+docker run -it --rm -v ${PWD}:/repronim17 -p 9990:9990 ${ProjName}
