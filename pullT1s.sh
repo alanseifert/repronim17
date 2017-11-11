713584a4c8a47e9a85682e72d8c6eadd81c7fdc6
Ely <benjamin.ely@mssm.edu>
2017-11-11 10:04:02 -0500

    firstpass pull T1 script

--- pullT1s.sh
+++ pullT1s.sh
@@ -5,7 +5,17 @@ mkdir -p Subjects/female Subjects/male
 subtable="" # INSERT CSV HERE
 
 for (( i=2 ; i<=31 ; i++ )) ; do
+	#starting at 2 to avoid header
 	line=`head -n $i subtable | tail -n 1`
-	sex=
-
+	age=cat $line | awk '{ print $4 }'
+	if [[ "$age" -eq 18 ]] ; then
+		sex=cat $line | awk '{ print $3 }'
+		url=cat $line | awk '{ print $5 }'
+		filename=${url##*/}
+		if [[ "$sex" == "F" ]] ; then
+			curl -o Subjects/female/$filename
+		else
+			curl -o Subjects/male/$filename
+		fi
+	fi
 done
